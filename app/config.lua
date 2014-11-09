4e69ac4b903222326b64fed9e4086b99a6ee49a3
Yusuke Shimizu <a.ryuklnm@gmail.com>
2014-11-10 01:47:10 +0900

    :sushi:

---
+++ app/config.lua
@@ -0,0 +1,9 @@
+local conf = {
+    ip_ban_threshold = 3,
+    user_lock_threshold = 3,
+    redis = {
+        host = "127.0.0.1",
+        port = 6379
+    }
+}
+return conf
--- app/post_login.lua
+++ app/post_login.lua
@@ -2,11 +2,12 @@ local util = require "utils"
 local json = require "resty.libcjson"
 local redis = require("resty.redis").new()
 local session = require("resty.session").start()
+local conf = require '../app/config'
 
 -- timeout: 1s
 redis:set_timeout(1000)
 
-local ok, err = redis:connect("127.0.0.1", 6379)
+local ok, err = redis:connect(conf.redis.host, conf.redis.port)
 if not ok then
     ngx.log(ngx.ERR, "failed to connect to redis: ", err)
     return ngx.exit(500)
@@ -23,9 +24,37 @@ function get_user(login)
     return redis:array_to_hash(res)
 end
 
+function ip_banned()
+    local kip = util:redis_key_ip(ngx.var.remote_addr)
+    local fail_count = tonumber(redis:get(kip))
+    if not fail_count then
+        return false
+    end
+    return fail_count >= conf.user_lock_threshold
+end
+
+function user_locked(login)
+    local kuser_fail = util:redis_key_user_fail(login)
+    local fail_count = tonumber(redis:get(kuser_fail))
+    if not fail_count then
+        return false
+    end
+    return fail_count >= conf.ip_ban_threshold
+end
+
 -- TODO: evalsha incr script
 function attempt_login(login, password)
     local user = get_user(login)
+    ngx.log(ngx.ERR, "** attempt login: ", login)
+
+    if ip_banned() then
+        ngx.log(ngx.ERR, "** ip bunned")
+        return _, "You're banned."
+    end
+    if user_locked(login) then
+        ngx.log(ngx.ERR, "** user locked")
+        return _, "This account is locked."
+    end
 
     local kip  = util:redis_key_ip(ngx.var.remote_addr)
     local kuser_fail = util:redis_key_user_fail(login)
@@ -35,8 +64,6 @@ function attempt_login(login, password)
         pcall(redis:rename(knext_last, klast))
         redis:hmset(knext_last, {at=ngx.localtime(), ip=ngx.var.remote_addr })
         redis:mset(kip, 0, kuser_fail, 0)
-        ngx.log(ngx.ERR, "** bunned ip count: ", redis:get(kip))
-        ngx.log(ngx.ERR, "** locked user count: ", redis:get(kuser_fail))
         return {login = user.login}
     else
         redis:incr(kip)
