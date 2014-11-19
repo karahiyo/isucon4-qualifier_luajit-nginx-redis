4f5d0da06ddc3db19ce5fe920bd98356dff8ccec
Yusuke Shimizu <a.ryuklnm@gmail.com>
2014-11-20 00:18:10 +0900

    :coffee:

--- app/app.lua
+++ app/app.lua
@@ -1,42 +1,87 @@
-local app = {}
+local app = {
+    redis = nil
+}
 
-function app:redis_key_user(login)
+function app:start(_host,_port)
+    self.redis = resty_redis:new()
+
+    -- timeout: 1s
+    self.redis:set_timeout(1000)
+
+    local ok, err = self.redis:connect(_host, _port)
+    if not ok then
+        ngx.log(ngx.ERR, "failed to connect to redis: ", err)
+        return ngx.exit(500)
+    end
+end
+
+function app:key_user(login)
     return "isu4:user:"..login
 end
-function app:redis_key_user_fail(login)
+function app:key_user_fail(login)
     return "isu4:user_fail:"..login
 end
-function app:redis_key_ip(ip)
+function app:key_ip(ip)
     return "isu4:ip:"..ip
 end
-function app:redis_key_last(login)
+function app:key_last(login)
     return "isu4:last:"..login
 end
-function app:redis_key_next_last(login)
+function app:key_next_last(login)
     return "ise4:next_last:"..login
 end
 function app:get_user(login)
-    local res = redis:hgetall(app:redis_key_user(login))
+    local res = self.redis:hgetall(app:key_user(login))
     if not res then
         return
     end
-    return redis:array_to_hash(res)
+    return self.redis:array_to_hash(res)
 end
 function app:ip_banned()
-    local kip = app:redis_key_ip(ngx.var.remote_addr)
-    local fail_count = tonumber(redis:get(kip))
+    local kip = app:key_ip(ngx.var.remote_addr)
+    local fail_count = tonumber(self.redis:get(kip))
     if not fail_count then
         return false
     end
     return fail_count >= conf.user_lock_threshold
 end
 function app:user_locked(login)
-    local kuser_fail = app:redis_key_user_fail(login)
-    local fail_count = tonumber(redis:get(kuser_fail))
+    local kuser_fail = app:key_user_fail(login)
+    local fail_count = tonumber(self.redis:get(kuser_fail))
     if not fail_count then
         return false
     end
     return fail_count >= conf.ip_ban_threshold
+end
+
+function app:attempt_login(login, password)
+    local user = app:get_user(login)
+
+    if app:ip_banned() then
+        ngx.log(ngx.ERR, "** ip bunned")
+        return _, "You're banned."
+    end
+    if self:user_locked(login) then
+        ngx.log(ngx.ERR, "** user locked")
+        return _, "This account is locked."
+    end
+
+    local kip  = app:key_ip(ngx.var.remote_addr)
+    local kuser_fail = app:key_user_fail(login)
+    if user and user.password == password then
+        local klast = app:key_last(login)
+        local knext_last = app:key_next_last(login)
+        pcall(self.redis:rename(knext_last, klast))
+        self.redis:hmset(knext_last, {at=ngx.localtime(), ip=ngx.var.remote_addr })
+        self.redis:mset(kip, 0, kuser_fail, 0)
+        return {login = user.login}
+    else
+        self.redis:incr(kip)
+        self.redis:incr(kuser_fail)
+        ngx.log(ngx.ERR, "** bunned ip count: ", self.redis:get(kip))
+        ngx.log(ngx.ERR, "** locked user count: ", self.redis:get(kuser_fail))
+        return _, "Wrong username or password"
+    end
 end
 
 return app
